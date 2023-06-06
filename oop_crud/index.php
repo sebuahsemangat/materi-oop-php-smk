d16857af72edc0e0075671f658597458586f49eb
sebuahsemangat <mrsyntax25@gmail.com>
2023-06-06 09:38:48 +0700

    Index File

---
+++ oop_crud/index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CRUD OOP PHP</title>
+</head>
+<body>
+    <form action="" method="post">
+        <table>
+            <tr>
+                <td>NIS</td>
+                <td>:</td>
+                <td><input type="text" name="nis" id=""></td>
+            </tr>
+            <tr>
+                <td>Nama</td>
+                <td>:</td>
+                <td><input type="text" name="nama" id=""></td>
+            </tr>
+            <tr>
+                <td>Kelas</td>
+                <td>:</td>
+                <td><input type="text" name="kelas" id=""></td>
+            </tr>
+            <tr>
+                <td></td>
+                <td></td>
+                <td><input type="submit" value="SUBMIT"></td>
+            </tr>
+        </table>
+    </form>
+</body>
+</html>
